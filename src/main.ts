4f8df7d42b64c869c1d32ad91f0b16b332d5668f
Badeeson Yongyuthmeechai <badeeson@Badeesons-MacBook-Air.local>
2023-12-02 17:54:27 +0700

    First commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'hero',
+      protoPath: join(__dirname, 'hero/hero.proto'),
+    },
+  });
+  await app.listen();
+}
+bootstrap();
