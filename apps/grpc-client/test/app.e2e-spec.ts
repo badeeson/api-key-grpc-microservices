798ba5dbf0e1854b1dcb03f7ebf0417cfd16318b
Badeeson <badeeson@gmail.com>
2023-12-04 03:09:10 +0700

    Update project name

--- apps/grpc-client/test/app.e2e-spec.ts
+++ apps/grpc-client/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { GrpcClientModule } from './../src/grpc-client.module';
+import { GrpcClientModule } from '../src/grpc-client.module';
 
 describe('GrpcClientController (e2e)', () => {
   let app: INestApplication;
